c5fb60b6a3aa54ff129f35b56e2caca5fb99c922
Yuliya Mysiura <Yuliya Mysiura>
2024-09-10 20:30:00 +0300

    first commit

---
+++ hello.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class HelloWorld {
+    public static void main(String[] args) {
+        int a, b;
+        try (Scanner in = new Scanner(System.in)) {
+            a = in.nextInt();
+            b = in.nextInt();
+        }
+        System.out.println(a + b);
+    }
+}
